c7804e1494b27b46861e809b80fd499e819bf571
Genildocs <genildocs@gmail.com>
2022-06-28 15:21:46 -0300

    primeiro commit

---
+++ src/collections/list/Array_List.java
@@ -0,0 +1,10 @@
+package collections.list;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class Array_List {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/collections/list/Linkelist.java
@@ -0,0 +1,22 @@
+package collections.list;
+import java.util.List;
+import java.util.LinkedList;
+public class Linkelist {
+    public static void main(String[] args) {
+
+        List<String > names = new LinkedList<String>();
+
+        names.add("Joao");
+        names.add("Maria");
+        names.add("Kaio");
+        names.add("Marcos");
+        System.out.println(names);
+
+        System.out.println(names.get(0)); // Pego pelo indice
+        System.out.println(names.contains("Kaio")); // Verifico se o elemento existe
+
+        names.set(0,"genildo"); // Adiciono um elemento substituindo outro pelo indicie.
+        System.out.println(names);
+
+    }
+}
